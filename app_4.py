9656d2cfc1aa0ffcde63ec1dc28fecca61e721f6
hit2025 <rik.project3@gmail.com>
2025-10-05 00:03:22 +0530

    Update app_4.py

--- app_4.py
+++ app_4.py
@@ -324,6 +324,7 @@ def home():
 # -------------------------
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8001))
+    port = int(os.environ.get("PORT", 8000))
     print(f"Starting on port {port} ...")
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
